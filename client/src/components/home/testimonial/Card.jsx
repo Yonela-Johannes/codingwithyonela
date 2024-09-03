93e30a575b43a9b9102f9a6f2d9dfcddc252ee1a
Yonela Johannes <johannesyonela@gmail.com>
2024-09-03 11:39:44 +0200

    fix: testimonial card styling

--- client/src/components/home/testimonial/Card.jsx
+++ client/src/components/home/testimonial/Card.jsx
@@ -23,7 +23,7 @@ const Card = ({ element, theme }) =>
 
     return (
         <div key={element?.id} className="w-full h-full 2 lg:w-1/4 px-4 py-2 pt-12">
-            <div className={`flex flex-col ${theme == "light" ? "bg-bg_lightest" : "bg-bg_grey"} h-full min-h-[180px] max-h-[180px] rounded-md text-md border border-neutral-800 font-thin`}>
+            <div className={`flex flex-col ${theme == "light" ? "bg-bg_lightest" : "bg-bg_grey"} h-full min-h-[160px] max-h-[160px] rounded-md p-4 lg:p-6 text-md border border-neutral-800 font-thin`}>
                 <p className="flex-1 text-sm lg:text-base">{element.message?.slice(0, 130)}...</p>
                 <div className="lg:flex items-start justify-between">
                     <div>
