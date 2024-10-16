0ef0e5972354e2904d7e31fc5b35445aec32d037
Yonela Johannes <johannesyonela@gmail.com>
2024-10-16 17:03:57 +0200

    fix: cards layout

--- client/src/pages/ProjectPage.jsx
+++ client/src/pages/ProjectPage.jsx
@@ -3,15 +3,17 @@ import moment from "moment";
 import { useNavigate } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-import {
-  disableMessageUpdate,
-  getAllprojects,
-} from "../features/project/projectSlice";
+import
+  {
+    disableMessageUpdate,
+    getAllprojects,
+  } from "../features/project/projectSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { PiHeartDuotone } from "react-icons/pi";
 import { cn } from "../lib/utils";
 
-export default function ProjectPage() {
+export default function ProjectPage()
+{
   const { theme } = useContext(ThemeContext);
   const { projects } = useSelector(
     (state) => state.project
@@ -19,12 +21,14 @@ export default function ProjectPage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const getProjects = () => {
+  const getProjects = () =>
+  {
     dispatch(getAllprojects());
     dispatch(disableMessageUpdate());
   };
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     getProjects();
   }, []);
 
@@ -34,20 +38,18 @@ export default function ProjectPage() {
         <div
           key={project?.id}
           onClick={() => navigate(`/projects/${project?.project_id}`)}
-          className={`${
-            theme == "light"
+          className={`${theme == "light"
               ? "bg-bg_lightest text-bg_primary hover:bg-white"
               : "bg-bg_core border-bg_core text-bg_lightest hover:bg-bg_gray"
-          } text-sm lg:text-base flex flex-col gap-2 border p-2 lg:p-4 mb-3 cursor-pointer hover:shadow-lg duration-300 border-cl_primary shadow-cl_primary shadow-[5px_5px_0px_0px_#6c6c6c]`}
+            } text-sm lg:text-base flex flex-col gap-2 border p-2 lg:p-4 mb-3 cursor-pointer hover:shadow-lg duration-300 border-cl_primary shadow-cl_primary shadow-[5px_5px_0px_0px_#6c6c6c]`}
         >
           <div
-            className={`flex flex-1 items-start justify-between${
-              theme == "light"
+            className={`flex flex-1 items-start justify-between${theme == "light"
                 ? "bg-white text-bg_primary border-b-2"
                 : "bg-bg_core  border-b-2 border-bg_grey"
-            }`}
+              }`}
           >
-            <div className={`flex items-start mb-1 space-x-2 py-2 `}>
+            <div className={`flex items-start flex-col lg:flex-row mb-1 space-x-2 py-2 `}>
               <div className="bg-clr_alt h-16 w-16 md:h-20 md:w-20 flex items-center justify-center rounded-full relative">
                 <div className="aspect-square w-full h-full">
                   <img
